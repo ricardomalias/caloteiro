5bacbcbbacaf3fd3761c47df766840e5e771958f
ruindows\ricar <ricardomalias@gmail.com>
2019-07-11 01:28:29 -0300

    Updates

---
+++ precache-manifest.79f3e955891efe79f98ef25f8942f4ac.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35966c1fa6ae75e79cbce5a2bd51a1ec",
+    "url": "/caloteiro/index.html"
+  },
+  {
+    "revision": "0ed8bec8e4466014c036",
+    "url": "/caloteiro/static/css/main.34112e4c.chunk.css"
+  },
+  {
+    "revision": "117ba1c59c6d3eefbc03",
+    "url": "/caloteiro/static/js/2.b9c9f3fd.chunk.js"
+  },
+  {
+    "revision": "0ed8bec8e4466014c036",
+    "url": "/caloteiro/static/js/main.ed3d9d51.chunk.js"
+  },
+  {
+    "revision": "a22a8b09e93908ab5206",
+    "url": "/caloteiro/static/js/runtime~main.041b952d.js"
+  }
+]);
